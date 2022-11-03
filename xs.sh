059208c830116cbf8ea8452c5bf5c9368664caba
13-05 <1305@national.shitposting.agency>
2022-11-03 18:56:40 -0500

    make syntax and stuff more uniform

--- xs.sh
+++ xs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 _get_package_manager () {
-  set -- xbps-install pacman apt
+  set -- "xbps-install" "pacman" "apt"
 
   for package_manager in "$@"; do
     if command -v "$package_manager" 2>/dev/null >&2; then
@@ -10,41 +10,41 @@ _get_package_manager () {
   done
 }
 
-PACKAGE_MANAGER=$(_get_package_manager)
+PACKAGE_MANAGER="$(_get_package_manager)"
 
 _get_available_packages () {
-  case $PACKAGE_MANAGER in
-    xbps-install) xpkg -a ;;
-    pacman) pacman -Slq ;;
-    apt) apt-cache pkgnames --generate ;;
+  case "$PACKAGE_MANAGER" in
+    "xbps-install") xpkg -a ;;
+    "pacman") pacman -Slq ;;
+    "apt") apt-cache pkgnames --generate ;;
   esac
 }
 
 _get_installed_packages () {
-  case $PACKAGE_MANAGER in
-    xbps-install) xpkg ;;
-    pacman) pacman -Qq ;;
-    apt) dpkg --get-selections | sed 's:install$::' ;;
+  case "$PACKAGE_MANAGER" in
+    "xbps-install") xpkg ;;
+    "pacman") pacman -Qq ;;
+    "apt") dpkg --get-selections | sed 's:install$::' ;;
   esac
 }
 
 _do_installer () {
   package_list="$(_get_available_packages)"
 
-  case $PACKAGE_MANAGER in
-    xbps-install) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "xq {1}" | xargs -ro sudo xbps-install ;;
-    pacman) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "pacman -Si {1}" | xargs -ro sudo pacman -S ;;
-    apt) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "apt-cache show {1}" | xargs -ro sudo apt install ;;
+  case "$PACKAGE_MANAGER" in
+    "xbps-install") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "xq {1}" | xargs -ro sudo xbps-install ;;
+    "pacman") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "pacman -Si {1}" | xargs -ro sudo pacman -S ;;
+    "apt") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "apt-cache show {1}" | xargs -ro sudo apt install ;;
   esac
 }
 
 _do_uninstaller () {
   package_list="$(_get_installed_packages)"
 
-  case $PACKAGE_MANAGER in
-    xbps-install) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "xq {1}" | xargs -ro sudo xbps-remove ;;
-    pacman) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "pacman -Si {1}" | xargs -ro sudo pacman -R ;;
-    apt) echo "$package_list" | fzf -m --preview-window=right:66%:wrap --preview "apt-cache show {1}" | xargs -ro sudo apt remove ;;
+  case "$PACKAGE_MANAGER" in
+    "xbps-install") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "xq {1}" | xargs -ro sudo xbps-remove ;;
+    "pacman") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "pacman -Si {1}" | xargs -ro sudo pacman -R ;;
+    "apt") echo "$package_list" | fzf -m --preview-window="right:66%:wrap" --preview "apt-cache show {1}" | xargs -ro sudo apt remove ;;
   esac
 }
 
@@ -55,6 +55,7 @@ _help() {
   echo "OPTIONS"
   echo " -i --install    Select packages to install"
   echo " -r --remove     Select packages to uninstall"
+  echo " -h --help       Prints this message"
 }
 
 main () {
